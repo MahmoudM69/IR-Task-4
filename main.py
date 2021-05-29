0c270893d68c66ea4db887dd4cad1b13a61e1c58
MahmoudM69 <blabla96322@gmail.com>
2021-05-29 22:32:11 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,64 @@
+try:
+    # Fix UTF8 output issues on Windows console.
+    # Does nothing if package is not installed
+    from win_unicode_console import enable
+    enable()
+except ImportError:
+    pass
+import re
+import nltk
+from arabic_reshaper import ArabicReshaper
+from langdetect import detect
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+
+configuration = {
+    'delete_harakat': True,
+    'support_ligatures': True,
+    'RIAL SIGN': True,
+}
+reshaper = ArabicReshaper(configuration=configuration)
+
+# Read Doc
+with open("Doc1.txt", encoding="utf-8") as D1:
+    D1 = D1.read()
+print('')
+print('Your text:\r\n')
+print(D1)
+
+# Language detector
+lang = detect(D1)
+if lang == "en":
+    lang = 'english'
+elif lang == "ar":
+    lang = 'arabic'
+elif lang == "fr":
+    lang = 'french'
+elif lang == "de":
+    lang = 'german'
+
+# Remove punctuation
+D1 = re.sub(r'[^\w\s]', '', D1)
+
+# Reduce all letters to lower case
+D1 = D1.lower()
+
+# with using tokens. ps: Arabic can not be tokenized by nltk
+nltk.download('punkt')
+D1 = word_tokenize(D1, ('english' if lang == 'arabic' else lang))
+
+# Importing stopwords
+nltk.download('stopwords')
+stop_words = set(stopwords.words(lang))
+
+# Remove stop words
+result = [i for i in D1 if not i in stop_words]
+
+# Sorting
+# result.sort()
+
+# Printing
+print('')
+print('Your text after compiling:\r\n')
+print(result)
